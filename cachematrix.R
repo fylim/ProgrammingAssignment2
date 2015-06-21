1019299b83f51b36cd8c4c9cbc6a09d718d402ab
fylim <feiyau.lim@gmail.com>
2015-06-22 01:10:33 +0800

    Minor updates to description
    No code change.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
-## This second programming assignment will require you to write an R function that is able to cache 
-## potentially time-consuming computations. For example, taking the mean of a numeric vector is 
-## typically a fast operation. However, for a very long vector, it may take too long to compute the mean, 
-## especially if it has to be computed repeatedly (e.g. in a loop). If the contents of a vector are not changing, 
-## it may make sense to cache the value of the mean so that when we need it again, it can be looked up in the 
-## cache rather than recomputed. 
+## This R functions are meant to cache potentially time-consuming computations. 
+## For example, taking the inverse of a numeric vector is typically a fast operation. However, for a very long vector, 
+## it may take too long to compute the inverse, especially if it has to be computed repeatedly (e.g. in a loop).
+## If the contents of a vector are not changing, it may make sense to cache the value of the inverse so that 
+## when we need it again, it can be looked up in the cache rather than recomputed. 
+
 
 ## The first function, makeVector creates a special "vector", which is really a list containing a function to
 
@@ -17,22 +17,23 @@ makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
 
   set <- function(x) {  
-    x <<- y
-    i <<- NULL
-  }
+   x <<- y
+   i <<- NULL
+ }
   get <- function() x
   setinversed <- function(inversed) i <<- inversed
   getinversed <- function() i
-  list(set = set, get = get,
+  list(set = set, 
+       get = get,
        setinversed = setinversed,
        getinversed = getinversed)
 }
 
 
-## The following function calculates the mean of the special "vector" created with the above function. 
-## However, it first checks to see if the mean has already been calculated. If so, it gets the mean from 
-## the cache and skips the computation. Otherwise, it calculates the mean of the data and sets the value 
-## of the mean in the cache via the setmean function.
+## The following function calculates the inverse of the special "vector" created with the above function. 
+## However, it first checks to see if the mean has already been calculated. If so, it gets the inverse from 
+## the cache and skips the computation. Otherwise, it calculates the inverse of the data and sets the value 
+## of the inverse in the cache via the setinversed function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -48,3 +49,4 @@ cacheSolve <- function(x, ...) {
     i
   }
   
+
